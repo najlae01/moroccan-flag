d42247e8b25a79f025f1aa6df016fc411b26cf12
najlae01 <najlae.abarghache1@gmail.com>
2023-07-23 02:31:56 +0100

    second commit

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,11 +1,13 @@
 precision mediump float;
 
-varying float vRandom;
+// varying float vRandom;
 varying vec2 vUv;
+varying float vElevation;
 uniform sampler2D uTexture;
 
 void main()
 {
     vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb += vElevation * 1.5 + 0.025;
     gl_FragColor = textureColor;
 }
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -8,17 +8,19 @@
   attribute float aRandom;
   attribute vec2 uv;
 
-  varying float vRandom;
+//   varying float vRandom;
   varying vec2 vUv;
+  varying float vElevation;
 
   void main()
   {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-    modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime * 0.1) * 0.1;
-    // modelPosition.z += aRandom * 0.1;
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime * 0.1) * 0.1;
 
+    modelPosition.z += elevation;
+    // modelPosition.z += aRandom * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
@@ -27,6 +29,7 @@
 
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0); 
 
-    vRandom = aRandom;
+    // vRandom = aRandom;
     vUv = uv;
+    vElevation = elevation;
   }
